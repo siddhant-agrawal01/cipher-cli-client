24bba6a9cca910284ffe434b7ca0ead39bdab9f5
siddhant agrawal <siddhant-agrawal01>
2025-11-22 18:38:49 +0530

    add the dark theme

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default page;
