b1bbcf03aa7249612d2eb9b0527d6581f2154ad8
siddhant agrawal <siddhant-agrawal01>
2025-11-22 18:59:06 +0530

    fix the login issue

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,12 +1,30 @@
-import { LoginForm } from "@/components/login-form";
-import React from "react";
 
-const page = () => {
+"use client"
+import { LoginForm } from '@/components/login-form'
+import { Spinner } from '@/components/ui/spinner';
+import { authClient } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const Page =  () => {
+    const {data , isPending , } = authClient.useSession();
+  const router = useRouter();
+  if(isPending){
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+          <Spinner />
+      </div>
+    )
+  }
+
+  if(data?.session && data?.user){
+    router.push("/")
+  }
   return (
-    <>
-      <LoginForm />
-    </>
-  );
-};
 
-export default page;
+    <LoginForm />
+
+  )
+}
+
+export default Page
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3005",
+    baseURL: "http://localhost:5000",
     
   //     plugins: [ 
   //   deviceAuthorizationClient(), 
